ef92277f82c1fbba87e99a9f76761a6b2c612f1b
s12cybersecurity <ssalssa.1.12.2.1.2.1@gmail.com>
2023-11-03 00:40:04 +0100

    headers information

--- IATHookingImplant/IATHookingImplant/IATHookingImplant.cpp
+++ IATHookingImplant/IATHookingImplant/IATHookingImplant.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <Windows.h>
 
-int main(){
-    std::cout << "Hello World!\n";
+using namespace std;
+
+IMAGE_IMPORT_DESCRIPTOR* getImportTable() {
     DWORD baseAddress = (DWORD)GetModuleHandle(NULL);
-    std::cout << "Base Address: " << baseAddress << std::endl;
-    std::getchar();
+    IMAGE_DOS_HEADER* dosHeader = (IMAGE_DOS_HEADER*) baseAddress;
+    IMAGE_NT_HEADERS* ntHeader = (IMAGE_NT_HEADERS*)((DWORD)baseAddress + (*dosHeader).e_lfanew);
+    IMAGE_OPTIONAL_HEADER optionalHeader = (IMAGE_OPTIONAL_HEADER)(*ntHeader).OptionalHeader;
+    IMAGE_DATA_DIRECTORY importTableDesc = optionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
+    IMAGE_IMPORT_DESCRIPTOR* importTable = (IMAGE_IMPORT_DESCRIPTOR*)((DWORD)baseAddress + importTableDesc.VirtualAddress);
+    return importTable;
+}
+
+int main(){
+    
 }
 
